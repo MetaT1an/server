73f2703e0845f3bb8c363ccc951be1f4cfe72b8d
Yuchen Tian <tyc896@qq.com>
2019-03-25 10:27:04 +0800

    [add] models

--- models.py
+++ models.py
@@ -14,3 +14,34 @@ class User(db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.pwdhash, password)
+
+
+class Policy(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    pname = db.Column(db.String(20), unique=True, nullable=False)
+    description = db.Column(db.String(100), unique=False, nullable=True)
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    uid = db.Column(db.Integer, nullable=False)
+    tname = db.Column(db.String(35), nullable=False)
+    date = db.Column(db.String(20))
+    status = db.Column(db.Integer)      # 0 for undo, 1 for scanning, 2 for done
+
+
+class Host(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tid = db.Column(db.Integer)     # task id, define while creating
+    hname = db.Column(db.String(35), nullable=False)
+    status = db.Column(db.String(15))
+    policy = db.Column(db.String(15))   # define while creating
+    start = db.Column(db.String(20))
+    end = db.Column(db.String(20))
+    elapse = db.Column(db.String(15))
+    target = db.Column(db.String(20))   # define while creating
+    critical = db.Column(db.Integer)
+    high = db.Column(db.Integer)
+    medium = db.Column(db.Integer)
+    low = db.Column(db.Integer)
+    info = db.Column(db.Integer)
